644a8097d5d6e412293d9ffc2149794d42763de2
florianmarkusse <florianmarkusse@hotmail.com>
2025-02-25 22:24:41 +0100

    iwyu: x86

--- projects/x86/code/fault/print/src/core.c
+++ projects/x86/code/fault/print/src/core.c
@@ -1,9 +1,9 @@
 #include "x86/cpu/status/core.h"
 
 #include "abstraction/log.h"
-#include "shared/text/string.h"
+#include "abstraction/text/converter/base.h"
 #include "shared/enum.h"
-#include "abstraction/text/converter/converter.h"
+#include "shared/text/string.h"
 
 static string faultToString[CPU_FAULT_COUNT] = {CPU_FAULT_ENUM(ENUM_TO_STRING)};
 
