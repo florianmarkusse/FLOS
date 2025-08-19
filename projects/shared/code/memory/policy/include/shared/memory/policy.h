9db217cead7480ed0b20fddb0ed88a9d2c8f7812
florianmarkusse <florianmarkusse@hotmail.com>
2025-08-19 21:23:43 +0200

    kernel attributes

--- projects/shared/code/memory/policy/include/shared/memory/policy.h
+++ projects/shared/code/memory/policy/include/shared/memory/policy.h
@@ -4,13 +4,15 @@
 #include "shared/memory/management/definitions.h"
 #include "shared/types/numeric.h"
 
-void *allocateIdentityMemory(U64 bytes, U64 align);
+__attribute__((malloc, alloc_align(2))) void *allocateIdentityMemory(U64 bytes,
+                                                                     U64 align);
 void freeIdentityMemory(Memory memory);
 
 // align should be a power of 2. And bytes should be a multiple of align.
 // NOTE: pageSize to map for future extension should be at most the value of
 // align or smaller.
-void *allocateMappableMemory(U64 bytes, U64 align, U64 mappingSize);
+__attribute__((malloc, alloc_align(2))) void *
+allocateMappableMemory(U64 bytes, U64 align, U64 mappingSize);
 void freeMappableMemory(Memory memory);
 
 #endif
