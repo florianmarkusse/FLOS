cf78541459cdfe1ca6dcaf472b8cf987bcacd1de
florianmarkusse <florianmarkusse@hotmail.com>
2025-09-09 23:06:32 +0200

    we get into the kernel and unceremoniously crash

---
+++ projects/shared/code/memory/management/include/shared/memory/management/init.h
@@ -0,0 +1,7 @@
+#ifndef SHARED_MEMORY_MANAGEMENT_INIT_H
+#define SHARED_MEMORY_MANAGEMENT_INIT_H
+
+#include "efi-to-kernel/kernel-parameters.h"
+void initMemoryManagers(KernelMemory *kernelMemory);
+
+#endif
